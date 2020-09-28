a9408e01eea38927c13ba33d180937b0a9c8e335
FileOtter <kevin.t.connell@gmail.com>
2020-09-28 18:11:00 -0400

    added employee objects

--- Develop/lib/Employee.js
+++ Develop/lib/Employee.js
@@ -1,1 +1,22 @@
-// TODO: Write code to define and export the Employee class
+class Employee  {
+    constructor (name , id , email,){
+        
+        this.name = name
+        this.id = id
+        this.email = email
+    }
+
+    getName() {
+        return this.name
+    }
+
+    getRole() {
+        return this.role || "Employee"
+    }
+
+    getEmail() {
+        return this.email
+    }
+}
+
+module.exports = Employee
--- Develop/lib/Engineer.js
+++ Develop/lib/Engineer.js
@@ -1,1 +1,19 @@
-// TODO: Write code to define and export the Engineer class.  HINT: This class should inherit from Employee.
+const Employee = require("./Employee");
+
+class Engineer extends Employee {
+    
+    constructor (name , id , email, gitHub){
+
+        super(name, id, email,)
+
+        this.role = "Engineer"
+        this.gitHub = gitHub
+    }
+
+    getGithub() {
+        return this.gitHub
+    }
+
+}
+
+module.exports = Engineer
