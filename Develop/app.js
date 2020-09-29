fd86441a5d6897e254d41acd79cf7e13ca018573
FileOtter <kevin.t.connell@gmail.com>
2020-09-29 19:27:10 -0400

    got inquirer to work

--- Develop/app.js
+++ Develop/app.js
@@ -10,18 +10,162 @@ const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
 
+const employees = [];
+
+console.log("Please build your team!");
+
+function createTeam() {
+    inquirer.prompt(
+        [
+            {
+                type: "list",
+                name: "memberChoice",
+                message: "Which type of team member would you like to add?",
+                choices: [
+                    "Engineer",
+                    "Intern",
+                    "No more team members"
+                ]
+            }
+        ]
+    ).then(function(answer) {
+        switch (answer.memberChoice) {
+            case "Engineer":
+                addEngineer();//define this function
+                break;
+            case "Intern":
+                addIntern();
+                break;
+            default:
+                createOutput();
+                break;
+        }
+    });
+}
+
+function addEngineer() {
+    //ask the question
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your engineer's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID number?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?",
+        },
+        {
+            type: "input",
+            name: "github",
+            message: "What is their GitHub username?",
+        },
+    ])
+
+    //handle the promise 
+    .then(function(answer){
+
+        //create new engineer obj
+        const engineer = new Engineer (answer.name, answer.id, answer.email, answer.github);
+
+        //push to emplyee array
+        employees.push(engineer)
+
+        //invoke createTeam function
+        createTeam()
+
+    })
+};
+
+function addIntern() {
+    //ask the question
+
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is your intern's name?",
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is their employee ID number?",
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is their email address?",
+        },
+        {
+            type: "input",
+            name: "school",
+            message: "What school do they go to?",
+        },
+    ])
+
+    //handle the promise 
+    .then(function(answer){
+
+        //create new engineer obj
+        const intern = new Intern (answer.name, answer.id, answer.email, answer.school);
+
+        //push to emplyee array
+        employees.push(intern)
+
+        //invoke createTeam function
+        createTeam()
+    })
+};
+
+function createOutput() {
+    //check if output folder exists, if not create it
+    if (!fs.existsSync(OUTPUT_DIR)) {
+        fs.mkdirSync(OUTPUT_DIR);
+    }
+    //write the data into the team.html
+    const data = render(employees);
+    fs.writeFileSync(outputPath,data,"utf-8");
+}
 
 inquirer.prompt([
     {
         type: "input",
-        name: "username",
-        message: "What is your GitHub username?",
+        name: "name",
+        message: "What is your manager's name?",
+    },
+    {
+        type: "input",
+        name: "id",
+        message: "What is their employee ID number?",
+    },
+    {
+        type: "input",
+        name: "email",
+        message: "What is their email address?",
+    },
+    {
+        type: "input",
+        name: "officeNumber",
+        message: "What is their office number?",
     },
 ])
-    .then(function (answer) {
+    .then(function(mAnswers) {
+        const manager = new Manager (mAnswers.name, mAnswers.id, mAnswers.email, mAnswers.officeNumber);
 
+        employees.push(manager);
+
+        createTeam();
 
     });
+
+
 // and to create objects for each team member (using the correct classes as blueprints!)
 
 // After the user has input all employees desired, call the `render` function (required above) and pass in an array containing all employee objects; the `render` function will generate and return a block of HTML including templated divs for each employee!
